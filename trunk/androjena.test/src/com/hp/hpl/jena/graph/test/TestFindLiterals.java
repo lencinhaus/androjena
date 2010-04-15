1ee8c7063cf0d417cceddfb5650472a5f9c7df1d
lorecarra <lorecarra@d606fbb5-c153-6f1d-4536-aea580ca57b0>
2010-04-15 15:31:24 +0000

    androjena.test inital commit

---
+++ trunk/androjena.test/src/com/hp/hpl/jena/graph/test/TestFindLiterals.java
@@ -0,0 +1,224 @@
+/*
+ 	(c) Copyright 2005, 2006, 2007, 2008, 2009 Hewlett-Packard Development Company, LP
+ 	All rights reserved - see end of file.
+<<<<<<< TestFindLiterals.java
+ 	$Id: TestFindLiterals.java,v 1.2 2009/07/27 09:13:36 andy_seaborne Exp $
+=======
+ 	$Id: TestFindLiterals.java,v 1.2 2009/07/27 09:13:36 andy_seaborne Exp $
+>>>>>>> 1.4
+ */
+
+package com.hp.hpl.jena.graph.test;
+
+import java.util.Set;
+
+import junit.framework.TestSuite;
+
+import com.hp.hpl.jena.graph.Graph;
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.graph.impl.LiteralLabelFactory;
+import com.hp.hpl.jena.util.iterator.Map1;
+
+public class TestFindLiterals extends GraphTestBase {
+	public TestFindLiterals(String name) {
+		super(name);
+	}
+
+	static final Map1<Triple, Node> getObject = new Map1<Triple, Node>() {
+		public Node map1(Triple o) {
+			return o.getObject();
+		}
+	};
+
+	// public static TestFindLiterals aTest
+	// ( final String graph, final String size, final String search, final
+	// String results )
+	// {
+	// return new TestFindLiterals
+	// ( "TestFindLiterals: graph {" + graph
+	// + "} size " + size
+	// + " search " + search
+	// + " expecting {" + results + "}" )
+	// {
+	// @Override
+	// public void runBare()
+	// {
+	// Graph g = graphWith( graph );
+	// int n = Integer.parseInt( size );
+	// Node literal = NodeCreateUtils.create( search );
+	// //
+	// assertEquals( "graph has wrong size", n, g.size() );
+	// Set<Node> got = g.find( Node.ANY, Node.ANY, literal ).mapWith( getObject
+	// ).toSet();
+	// assertEquals( nodeSet( results ), got );
+	// }
+	// };
+	// }
+
+	private void aTest(final String graph, final String size,
+			final String search, final String results) {
+		Graph g = graphWith(graph);
+		int n = Integer.parseInt(size);
+		Node literal = NodeCreateUtils.create(search);
+		//
+		assertEquals("graph has wrong size", n, g.size());
+		Set<Node> got = g.find(Node.ANY, Node.ANY, literal).mapWith(getObject)
+				.toSet();
+		assertEquals(nodeSet(results), got);
+	}
+
+	public static TestSuite suite() {
+		TestSuite result = new TestSuite(TestFindLiterals.class);
+		//
+		return result;
+	}
+
+	public void testFloatVsDouble() {
+		Node A = NodeCreateUtils.create("'1'xsd:float");
+		Node B = NodeCreateUtils.create("'1'xsd:double");
+		assertFalse(A.equals(B));
+		assertFalse(A.sameValueAs(B));
+		assertFalse(B.sameValueAs(A));
+		assertFalse(A.matches(B));
+		assertFalse(B.matches(A));
+	}
+
+	public void testProgrammaticValues() {
+		Node ab = Node.createLiteral(LiteralLabelFactory.create(new Byte(
+				(byte) 42)));
+		Node as = Node.createLiteral(LiteralLabelFactory.create(new Short(
+				(short) 42)));
+		Node ai = Node.createLiteral(LiteralLabelFactory
+				.create(new Integer(42)));
+		Node al = Node.createLiteral(LiteralLabelFactory.create(new Long(42)));
+		Graph g = graphWith("");
+		Node SB = NodeCreateUtils.create("SB");
+		Node SS = NodeCreateUtils.create("SS");
+		Node SI = NodeCreateUtils.create("SI");
+		Node SL = NodeCreateUtils.create("SL");
+		Node P = NodeCreateUtils.create("P");
+		g.add(Triple.create(SB, P, ab));
+		g.add(Triple.create(SS, P, as));
+		g.add(Triple.create(SI, P, ai));
+		g.add(Triple.create(SL, P, al));
+		assertEquals(4, iteratorToSet(
+				g.find(Node.ANY, P, NodeCreateUtils.create("42"))).size());
+	}
+
+	public void test1() {
+		aTest("a P 'simple'", "1", "'simple'", "'simple'");
+	}
+
+	public void test2() {
+		aTest("a P 'simple'xsd:string", "1", "'simple'", "'simple'xsd:string");
+	}
+
+	public void test3() {
+		aTest("a P 'simple'", "1", "'simple'xsd:string", "'simple'");
+	}
+
+	public void test4() {
+		aTest("a P 'simple'xsd:string", "1", "'simple'xsd:string",
+				"'simple'xsd:string");
+	}
+
+	public void test5() {
+		aTest("a P 'simple'; a P 'simple'xsd:string", "2", "'simple'",
+				"'simple' 'simple'xsd:string");
+	}
+
+	public void test6() {
+		aTest("a P 'simple'; a P 'simple'xsd:string", "2",
+				"'simple'xsd:string", "'simple' 'simple'xsd:string");
+	}
+
+	public void test7() {
+		aTest("a P 1", "1", "1", "1");
+	}
+
+	public void test8() {
+		aTest("a P '1'xsd:float", "1", "'1'xsd:float", "'1'xsd:float");
+	}
+
+	public void test9() {
+		aTest("a P '1'xsd:double", "1", "'1'xsd:double", "'1'xsd:double");
+	}
+
+	public void test10() {
+		aTest("a P '1'xsd:float", "1", "'1'xsd:float", "'1'xsd:float");
+	}
+
+	public void test11() {
+		aTest("a P '1.1'xsd:float", "1", "'1'xsd:float", "");
+	}
+
+	public void test12() {
+		aTest("a P '1'xsd:double", "1", "'1'xsd:int", "");
+	}
+
+	public void test13() {
+		aTest("a P 'abc'rdf:XMLLiteral", "1", "'abc'", "");
+	}
+
+	public void test14() {
+		aTest("a P 'abc'", "1", "'abc'rdf:XMLLiteral", "");
+	}
+
+	// floats & doubles are not compatible
+	public void test15() {
+		aTest("a P '1'xsd:float", "1", "'1'xsd:double", "");
+	}
+
+	public void test16() {
+		aTest("a P '1'xsd:double", "1", "'1'xsd:float", "");
+	}
+
+	public void test17() {
+		//
+		aTest("a P 1", "1", "'1'", "");
+	}
+
+	public void test18() {
+		aTest("a P 1", "1", "'1'xsd:integer", "'1'xsd:integer");
+	}
+
+	public void test19() {
+		aTest("a P 1", "1", "'1'", "");
+	}
+
+	public void test20() {
+		aTest("a P '1'xsd:short", "1", "'1'xsd:integer", "'1'xsd:short");
+	}
+
+	public void test21() {
+		aTest("a P '1'xsd:int", "1", "'1'xsd:integer", "'1'xsd:int");
+
+	}
+}
+
+/*
+ * (c) Copyright 2005, 2006, 2007, 2008, 2009 Hewlett-Packard Development
+ * Company, LP All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution. 3. The name of the author may not
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
---
+++ trunk/androjena.test/src/it/polimi/dei/dbgroup/pedigree/androjena/test/TestHelper.java
@@ -0,0 +1,133 @@
+package it.polimi.dei.dbgroup.pedigree.androjena.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.util.Log;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.util.FileUtils;
+import com.ibm.icu.text.CharsetDetector;
+
+public final class TestHelper {
+	private TestHelper() {
+	}
+
+	public static final String sanitizeFileName(String filename) {
+		String old = new String(filename);
+		if (filename.startsWith("file:"))
+			filename = filename.substring("file:".length());
+		int start = 0;
+		while (filename.charAt(start) == '/')
+			start++;
+		filename = filename.substring(start);
+		log(old + " sanitized to " + filename);
+		return filename;
+	}
+	
+	private static final void log(String s) {
+		Log.d("TestHelper", s);
+	}
+
+	public static final String getResourceURL(String filename) {
+		String sanitized = sanitizeFileName(filename);
+		URL resUrl = TestHelper.class.getClassLoader().getResource(sanitized);
+		if (resUrl != null)
+			return resUrl.toString();
+		return sanitized;
+	}
+
+	public static final InputStream openResource(String filename) {
+		InputStream is = TestHelper.class.getClassLoader().getResourceAsStream(
+				sanitizeFileName(filename));
+		if (is == null)
+			throw new RuntimeException("file " + filename
+					+ " not found in class resources");
+		return is;
+	}
+
+	private static final Pattern XML_CHARSET_PATTERN = Pattern
+			.compile("^.*<\\?xml.*encoding=\"([^\"]+)\".*\\?>.*$");
+
+	public static interface StreamFactory {
+		public InputStream createStream() throws IOException;
+	}
+
+	public static final Reader getXMLReader(StreamFactory factory)
+			throws IOException {
+		CharsetDetector detector = new CharsetDetector();
+		Reader innerReader = detector.getReader(factory.createStream(), null);
+		if(innerReader == null) innerReader = new InputStreamReader(
+				factory.createStream());
+		BufferedReader reader = new BufferedReader(innerReader);
+		String s;
+		StringBuilder sb = new StringBuilder();
+		try {
+			while ((s = reader.readLine()) != null) {
+				sb.append(s);
+			}
+			reader.close();
+		} catch (IOException ex) {
+			throw new RuntimeException(
+					"an error occurred while detecting declared charset", ex);
+		}
+		s = sb.toString();
+		Matcher m = XML_CHARSET_PATTERN.matcher(s);
+		if (m.matches()) {
+			String encoding = m.group(1);
+			if (encoding != null) {
+				try {
+					Reader r = detector.getReader(factory.createStream(), encoding);
+					if(r == null) r = new InputStreamReader(factory.createStream(), encoding);
+					log("detected encoding " + encoding + " for XML stream");
+					return r;
+				} catch (UnsupportedEncodingException ex) {
+				}
+			}
+		}
+		
+		Reader r = detector.getReader(factory.createStream(), null);
+		if(r == null) r = new InputStreamReader(factory.createStream());
+		log("encoding not found");
+		return r;
+	}
+
+	public static final void dumpModel(Model m) {
+		dumpModel(m, "N-TRIPLE");
+	}
+
+	public static final void dumpModel(Model m, String lang) {
+		StringWriter w = new StringWriter();
+		m.write(w, lang);
+		w.flush();
+		Log.d("TestHelper.dumpModel", w.toString());
+	}
+	
+	public static final File copyFile(String filename) throws IOException {
+		final byte[] buffer = new byte[1024];
+		InputStream input = openResource(filename);
+		File tempFile = File.createTempFile(FileUtils.getBasename(filename), FileUtils.getFilenameExt(filename));
+		OutputStream output = new FileOutputStream(tempFile);
+		int read;
+		while((read = input.read(buffer)) != -1) {
+			output.write(buffer, 0, read);
+		}
+		input.close();
+		output.flush();
+		output.close();
+		log("created temp file " + tempFile.getAbsolutePath() + " for resource " + filename);
+		return tempFile;
+	}
+}
