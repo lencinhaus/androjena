798c5efe5ccbe28cfbd25003ddfa1c87e26da546
lorecarra <lorecarra@d606fbb5-c153-6f1d-4536-aea580ca57b0>
2010-05-17 23:52:07 +0000

    Adopted Apache License version 2. Added license notices at the beginning of files. Swapped Sun's implementation of java.rmi.server.UID with GNU classpath's. Removed org.apache.xerces.util.SymbolHash. Updated dist jar.

--- trunk/androjena/src/it/polimi/dei/dbgroup/pedigree/androjena/java/rmi/server/UID.java
+++ trunk/androjena/src/it/polimi/dei/dbgroup/pedigree/androjena/java/rmi/server/UID.java
@@ -1,248 +1,225 @@
-/*
- * @(#)UID.java	1.22 03/12/19
- *
- * Copyright 2004 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
+/* UID.java -- The unique object Id
+   Copyright (c) 2006 Free Software Foundation, Inc.
+
+This file is part of GNU Classpath.
+
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301 USA.
+
+Linking this library statically or dynamically with other modules is
+making a combined work based on this library.  Thus, the terms and
+conditions of the GNU General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this library give you
+permission to link this library with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module.  An independent module is a module which is not derived from
+or based on this library.  If you modify this library, you may extend
+this exception to your version of the library, but you are not
+obligated to do so.  If you do not wish to do so, delete this
+exception statement from your version. */
+
+
 package it.polimi.dei.dbgroup.pedigree.androjena.java.rmi.server;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.security.SecureRandom;
+import java.io.Serializable;
+import java.net.InetAddress;
 
 /**
- * A <code>UID</code> represents an identifier that is unique over time with
- * respect to the host it is generated on, or one of 2<sup>16</sup> "well-known"
- * identifiers.
+ * Represents the unique identifier over time for the host which has generated
+ * it. It contains time (when created), counter (the number of the UID
+ * creation order) and virtual machine id components. The UID can also be
+ * constructed specifying a "well known" identifier in the for of short:
+ * this identifier defines the UID uniqueness alone. 
  * 
- * <p>
- * The {@link #UID()} constructor can be used to generate an identifier that is
- * unique over time with respect to the host it is generated on. The
- * {@link #UID(short)} constructor can be used to create one of 2<sup>16</sup>
- * well-known identifiers.
- * 
- * <p>
- * A <code>UID</code> instance contains three primitive values:
- * <ul>
- * <li><code>unique</code>, an <code>int</code> that uniquely identifies the VM
- * that this <code>UID</code> was generated in, with respect to its host and at
- * the time represented by the <code>time</code> value (an example
- * implementation of the <code>unique</code> value would be a process
- * identifier), or zero for a well-known <code>UID</code>
- * <li><code>time</code>, a <code>long</code> equal to a time (as returned by
- * {@link System#currentTimeMillis()}) at which the VM that this
- * <code>UID</code> was generated in was alive, or zero for a well-known
- * <code>UID</code>
- * <li><code>count</code>, a <code>short</code> to distinguish <code>UID</code>s
- * generated in the same VM with the same <code>time</code> value
- * </ul>
- * 
- * <p>
- * An independently generated <code>UID</code> instance is unique over time with
- * respect to the host it is generated on as long as the host requires more than
- * one millisecond to reboot and its system clock is never set backward. A
- * globally unique identifier can be constructed by pairing a <code>UID</code>
- * instance with a unique host identifier, such as an IP address.
- * 
- * @author Ann Wollrath
- * @author Peter Jones
- * @version 1.22, 03/12/19
- * @since JDK1.1
+ * @author Audrius Meskauskas (audriusa@bioinformatics.org)
  */
-public final class UID implements java.io.Serializable {
+public final class UID
+    implements Serializable
+{
+  /**
+   * Use the serial version uid for interoperability.
+   */
+  private static final long serialVersionUID = 1086053664494604050L;
+ 
+  /**
+   * The UID counter (the ordinary number in the sequence of number of UID's,
+   * created during the recent millisecond). In the next millisecond, it 
+   * starts from the minimal value again. In the unlikely case of creating
+   * more than 65536 uids per millisecond the process pauses till the next
+   * ms.
+   */
+  private static short uidCounter = Short.MIN_VALUE;
+  
+  /**
+   * The time, when the last UID has been created.
+   */
+  private static long last;
 
-	private static final long ONE_SECOND = 1000; // in milliseconds
-	private static int hostUnique;
-	private static boolean hostUniqueSet = false;
+  /**
+   * This constant tries to be the unique identifier of the virtual machine.
+   */
+  private static final int machineId = getMachineId();
 
-	private static final Object lock = new Object();
-	private static long lastTime = System.currentTimeMillis();
-	private static short lastCount = Short.MIN_VALUE;
+  /**
+   * The UID number in the UID creation sequence.
+   */
+  private short count;
 
-	/** indicate compatibility with JDK 1.1.x version of class */
-	private static final long serialVersionUID = 1086053664494604050L;
+  /**
+   * Always gets the uniqueNr value.
+   */
+  private int unique;
 
-	/**
-	 * number that uniquely identifies the VM that this <code>UID</code> was
-	 * generated in with respect to its host and at the given time
-	 * 
-	 * @serial
-	 */
-	private final int unique;
+  /**
+   * The time stamp, when the UID was created.
+   */
+  private long time;
+  
+  /**
+   * Create the new UID that would have the described features of the
+   * uniqueness.
+   */
+  public UID()
+  {
+    synchronized (UID.class)
+      {
+        time = System.currentTimeMillis();
+        unique = machineId;
+        if (time > last)
+          {
+            last = time;
+            count = uidCounter = Short.MIN_VALUE;
+          }
+        else
+          {
+            if (uidCounter == Short.MAX_VALUE)
+              {
+                // Make a 2 ms pause if the counter has reached the maximal
+                // value. This should seldom happen.
+                try
+                  {
+                    Thread.sleep(2);
+                  }
+                catch (InterruptedException e)
+                  {
+                  }
+                uidCounter = Short.MIN_VALUE;
+                time = last = System.currentTimeMillis();
+              }
+            count = ++uidCounter;
+          }
+      }
+  }
+  
+  /**
+   * Create the new UID with the well known id (number). All UIDs, creates
+   * with the this constructor having the same parameter are equal to each
+   * other (regardless to the host and time where they were created.
+   * 
+   * @param wellKnownId the well known UID.
+   */
+  public UID(short wellKnownId)
+  {
+    unique = wellKnownId;
+  }
+  
+  /**
+   * Get the hashCode of this UID.
+   */
+  public int hashCode()
+  {
+    return (int) (unique ^ time ^ count);
+  }
+  
+  /**
+   * Compare this UID with another UID for equality (not equal to other types of
+   * objects).
+   */
+  public boolean equals(Object other)
+  {
+    if (other instanceof UID)
+      {
+        UID ui = (UID) other;
+        return unique == ui.unique && time == ui.time && count == ui.count;
+      }
+    else
+      return false;
+  }
+  
+  public static UID read(DataInput in) throws IOException
+  {
+    UID uid = new UID();
+    uid.unique = in.readInt();
+    uid.time = in.readLong();
+    uid.count = in.readShort();
+    return (uid);
+  }
 
-	/**
-	 * a time (as returned by {@link System#currentTimeMillis()}) at which the
-	 * VM that this <code>UID</code> was generated in was alive
-	 * 
-	 * @serial
-	 */
-	private final long time;
+  public void write(DataOutput out) throws IOException
+  {
+    out.writeInt(unique);
+    out.writeLong(time);
+    out.writeShort(count);
+  }
 
-	/**
-	 * 16-bit number to distinguish <code>UID</code> instances created in the
-	 * same VM with the same time value
-	 * 
-	 * @serial
-	 */
-	private final short count;
+  /**
+   * Do our best to get the Id of this virtual machine.
+   */
+  static int getMachineId()
+  {
+    int hostIpHash;
 
-	/**
-	 * Generates a <code>UID</code> that is unique over time with respect to the
-	 * host that it was generated on.
-	 */
-	public UID() {
+    try
+      {
+        // Try to get the host IP.
+        String host = InetAddress.getLocalHost().toString();
+        // This hash is content - based, not the address based.
+        hostIpHash = host.hashCode();
+      }
+    catch (Exception e)
+      {
+        // Failed due some reason.
+        hostIpHash = 0;
+      }
 
-		synchronized (lock) {
-			if (!hostUniqueSet) {
-				hostUnique = (new SecureRandom()).nextInt();
-				hostUniqueSet = true;
-			}
-			unique = hostUnique;
-			if (lastCount == Short.MAX_VALUE) {
-				boolean done = false;
-				while (!done) {
-					long now = System.currentTimeMillis();
-					if (now < lastTime + ONE_SECOND) {
-						// pause for a second to wait for time to change
-						try {
-							Thread.sleep(ONE_SECOND);
-						} catch (java.lang.InterruptedException e) {
-						} // ignore exception
-						continue;
-					} else {
-						lastTime = now;
-						lastCount = Short.MIN_VALUE;
-						done = true;
-					}
-				}
-			}
-			time = lastTime;
-			count = lastCount++;
-		}
-	}
-
-	/**
-	 * Creates a "well-known" <code>UID</code>.
-	 * 
-	 * There are 2<sup>16</sup> possible such well-known ids.
-	 * 
-	 * <p>
-	 * A <code>UID</code> created via this constructor will not clash with any
-	 * <code>UID</code>s generated via the no-arg constructor.
-	 * 
-	 * @param num
-	 *            number for well-known <code>UID</code>
-	 */
-	public UID(short num) {
-		unique = 0;
-		time = 0;
-		count = num;
-	}
-
-	/**
-	 * Constructs a <code>UID</code> given data read from a stream.
-	 */
-	private UID(int unique, long time, short count) {
-		this.unique = unique;
-		this.time = time;
-		this.count = count;
-	}
-
-	/**
-	 * Returns the hash code value for this <code>UID</code>.
-	 * 
-	 * @return the hash code value for this <code>UID</code>
-	 */
-	public int hashCode() {
-		return (int) time + (int) count;
-	}
-
-	/**
-	 * Compares the specified object with this <code>UID</code> for equality.
-	 * 
-	 * This method returns <code>true</code> if and only if the specified object
-	 * is a <code>UID</code> instance with the same <code>unique</code>,
-	 * <code>time</code>, and <code>count</code> values as this one.
-	 * 
-	 * @param obj
-	 *            the object to compare this <code>UID</code> to
-	 * 
-	 * @return <code>true</code> if the given object is equivalent to this one,
-	 *         and <code>false</code> otherwise
-	 */
-	public boolean equals(Object obj) {
-		if (obj instanceof UID) {
-			UID uid = (UID) obj;
-			return (unique == uid.unique && count == uid.count && time == uid.time);
-		} else {
-			return false;
-		}
-	}
-
-	/**
-	 * Returns a string representation of this <code>UID</code>.
-	 * 
-	 * @return a string representation of this <code>UID</code>
-	 */
-	public String toString() {
-		return Integer.toString(unique, 16) + ":" + Long.toString(time, 16)
-				+ ":" + Integer.toString(count, 16);
-	}
-
-	/**
-	 * Marshals a binary representation of this <code>UID</code> to a
-	 * <code>DataOutput</code> instance.
-	 * 
-	 * <p>
-	 * Specifically, this method first invokes the given stream's
-	 * {@link DataOutput#writeInt(int)} method with this <code>UID</code>'s
-	 * <code>unique</code> value, then it invokes the stream's
-	 * {@link DataOutput#writeLong(long)} method with this <code>UID</code>'s
-	 * <code>time</code> value, and then it invokes the stream's
-	 * {@link DataOutput#writeShort(int)} method with this <code>UID</code>'s
-	 * <code>count</code> value.
-	 * 
-	 * @param out
-	 *            the <code>DataOutput</code> instance to write this
-	 *            <code>UID</code> to
-	 * 
-	 * @throws IOException
-	 *             if an I/O error occurs while performing this operation
-	 */
-	public void write(DataOutput out) throws IOException {
-		out.writeInt(unique);
-		out.writeLong(time);
-		out.writeShort(count);
-	}
-
-	/**
-	 * Constructs and returns a new <code>UID</code> instance by unmarshalling a
-	 * binary representation from an <code>DataInput</code> instance.
-	 * 
-	 * <p>
-	 * Specifically, this method first invokes the given stream's
-	 * {@link DataInput#readInt()} method to read a <code>unique</code> value,
-	 * then it invoke's the stream's {@link DataInput#readLong()} method to read
-	 * a <code>time</code> value, then it invoke's the stream's
-	 * {@link DataInput#readShort()} method to read a <code>count</code> value,
-	 * and then it creates and returns a new <code>UID</code> instance that
-	 * contains the <code>unique</code>, <code>time</code>, and
-	 * <code>count</code> values that were read from the stream.
-	 * 
-	 * @param in
-	 *            the <code>DataInput</code> instance to read <code>UID</code>
-	 *            from
-	 * 
-	 * @return unmarshalled <code>UID</code> instance
-	 * 
-	 * @throws IOException
-	 *             if an I/O error occurs while performing this operation
-	 */
-	public static UID read(DataInput in) throws IOException {
-		int unique = in.readInt();
-		long time = in.readLong();
-		short count = in.readShort();
-		return new UID(unique, time, count);
-	}
+    // Should be the unque address if hashcodes are addresses.
+    // Additionally, add the time when the RMI system was probably started
+    // (this class was first instantiated).
+    return new Object().hashCode() ^ (int) System.currentTimeMillis()
+           ^ hostIpHash;
+  }
+  
+    /**
+   * Get the string representation of this UID.
+   * 
+   * @return a string, uniquely identifying this id.
+   */
+  public String toString()
+  {
+    int max = Character.MAX_RADIX;
+    // Translate into object count, counting from 0.
+    long lc = (count - Short.MIN_VALUE) & 0xFFFF;
+    return Long.toString(unique, max) + ":" + Long.toString(time, max) + "."
+           + Long.toString(lc, max);
+  }
 }
